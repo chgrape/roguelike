47f7b26c7dcfd77bf939881b5df3c6648abab9c0
Jivko <jivkon1410@gmail.com>
2022-09-21 23:28:00 +0300

    updated player and entity classes

--- entity.py
+++ entity.py
@@ -4,6 +4,9 @@ class Entity:
         self._x = x
         self._y = y
         self.pos = (self._x, self._y)
+        self.velocity = 3
+        self.health = 100
+        self.sprite = 0
 
     def get_pos(self):
         return pos
@@ -13,4 +16,21 @@ class Entity:
         self._x = pos[0]
         self._y = pos[1]
 
+    def get_velocity(self):
+        return self.velocity
+
+    def set_velocity(self, velocity):
+        if velocity < 1:
+            self.velocity = 1
+        else:  
+            self.velocity = velocity
+
+    def get_health(self):
+        return self.health
+
+    def set_health(self, health):
+        if health < 0:
+            self.health = 0
+        else:
+            self.health = health
     
--- player.py
+++ player.py
@@ -1,9 +1,13 @@
+import pygame
 from entity import Entity
 
 class Player(Entity):
     def __init__(self, x, y, name): 
         Entity.__init__(self, x, y)
         self.name = name
+        self.mana = 100
+        self.sprite = pygame.image.load("Sprites/player.png")
+        self.body = self.sprite.get_rect()
 
     def get_pos(self):
         return Entity.get_pos(self)
@@ -16,3 +20,33 @@ class Player(Entity):
     
     def set_name(self, name):
         self.name = name
+
+    def get_velocity(self):
+        return Entity.get_velocity(self)
+
+    def set_velocity(self, velocity):
+        Entity.set_velocity(self, velocity)
+
+    def get_health(self):
+        return Entity.get_health(self)
+
+    def set_health(self, health):
+        Entity.set_health(self, health)
+
+    def get_mana(self):
+        return self.mana
+
+    def set_mana(self, mana):
+        if mana < 0:
+            self.mana = 0
+        else:
+            self.mana = mana
+
+    def move(self): # and sprint
+        pass
+
+    def draw(self, win): # ?
+        win.blit(self.sprite, self.body)
+    
+
+    
